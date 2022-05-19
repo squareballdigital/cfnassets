9f3731bfcd6925dd1bc21f15e6fde1c7fbcec523
Gordon Leigh <gordon@squareball.co>
2022-05-19 20:50:48 +0200

    fix: assets not added to template

--- src/template/AssetContext.ts
+++ src/template/AssetContext.ts
@@ -4,10 +4,10 @@ export class AssetContext {
   public readonly assets: AssetDefinition[] = [];
 
   public addAsset(name: string, value: AssetDefinition): boolean {
-    const exists = !!this.assets.find((x) => x.name === name);
-    if (!exists) {
+    const add = !this.assets.find((x) => x.name === name);
+    if (add) {
       this.assets.push(value);
     }
-    return exists;
+    return add;
   }
 }
